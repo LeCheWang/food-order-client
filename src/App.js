e9f5b083bf71eca1dff96d480a17aa3d51bd559a
LeCheWang <haphonggoroki@gmail.com>
2024-01-11 16:37:27 +0700

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-[#333]">
-      Hello world!
-    </h1>
-  )
+    <h1 className="text-3xl font-bold underline bg-[#333]">Hello world!</h1>
+  );
 }
