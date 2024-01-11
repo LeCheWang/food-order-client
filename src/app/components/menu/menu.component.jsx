4870c12377081b51025c674ffc6447763c589b21
LeCheWang <haphonggoroki@gmail.com>
2024-01-11 18:55:11 +0700

    css for menu

--- src/app/components/menu/menu.component.jsx
+++ src/app/components/menu/menu.component.jsx
@@ -4,11 +4,11 @@ function Menu(){
   const navigate = useNavigate();
 
   return (
-    <ul className="flex justify-around">
-      <li onClick={() => navigate("/")}>Home</li>
-      <li onClick={() => navigate("/cart")}>Cart</li>
-      <li onClick={() => navigate("/order")}>Order</li>
-      <li onClick={() => navigate("/profile")}>Profile</li>
+    <ul className="flex text-center justify-around bg-[#4bec81] h-[50px] leading-[50px] text-white cursor-pointer">
+      <li className="flex-1 transition-all border-r hover:text-[20px]" onClick={() => navigate("/")}>Home</li>
+      <li className="flex-1 transition-all border-r hover:text-[20px]" onClick={() => navigate("/cart")}>Cart</li>
+      <li className="flex-1 transition-all border-r hover:text-[20px]" onClick={() => navigate("/order")}>Order</li>
+      <li className="flex-1 transition-all hover:text-[20px]" onClick={() => navigate("/profile")}>Profile</li>
     </ul>
   )
 }
